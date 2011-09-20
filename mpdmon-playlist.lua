02de2e43731faa9a60b3bf574d42b25c2184773f
Jens Oliver John <jens.o.john@gmail.com>
2011-09-20 13:37:09 +0200

    Fixed error with empty playlist

--- mpdmon-playlist.lua
+++ mpdmon-playlist.lua
@@ -2,18 +2,16 @@
 -- For use with the wmii window manager
 -- Prints a formatted MPD playlist
 
--- Get MPD's position
 local p=io.popen("echo 'status' | nc 127.0.0.1 6600 | grep '^song:' | cut -d' ' -f2")
+local n=p:read("*l")
 
--- Read position and add 1, for mpd counts from 0, and mpc counts from 1
-local n=tonumber(p:read("*l"))+1
+n = n and (n + 1) or 0
+if n > 0 then
+    l=io.popen("mpc playlist | sed '/./=' | sed '/./N; s/\\n/ /' | sed '" .. n .. "s/.*/\\n&\\n/'")
+    print(l:read("*a"))
+    l:close()
+else
+    print("mpdmon: Playlist empty or connection error.")
+end
 
--- Prepare our playlist
-l=io.popen("mpc playlist | sed '/./=' | sed '/./N; s/\\n/ /' | sed '" .. n .. "s/.*/\\n&\\n/'")
-
--- Read playlist and print to stdout
-print(l:read("*a"))
-
--- Clean up the pipes, Watson
 p:close()
-l:close()
